d8474c3f8720909b3e3335639e67a7a81ac8205c
Default Cube <56493793+Quicksilver151@users.noreply.github.com>
2022-12-21 14:56:59 +0500

    added -r -R -H -o Functionality

--- src/main.rs
+++ src/main.rs
@@ -26,14 +26,38 @@ impl PrayerData{
         self.isha   = val[7];
     }
     
-    fn print_data(&self){
+    fn print_data(&self,time_format:TimeFormat){
         println!("Fajr:\t{}\nShuruq:\t{}\nDhuhur:\t{}\nAsr:\t{}\nMagrib:\t{}\nIsha:\t{}",
-                self.fajr   .minutes_to_time(),
-                self.sun    .minutes_to_time(),
-                self.dhuhur .minutes_to_time(),
-                self.asr    .minutes_to_time(),
-                self.magrib .minutes_to_time(),
-                self.isha   .minutes_to_time()
+                self.fajr   .minutes_to_time(&time_format),
+                self.sun    .minutes_to_time(&time_format),
+                self.dhuhur .minutes_to_time(&time_format),
+                self.asr    .minutes_to_time(&time_format),
+                self.magrib .minutes_to_time(&time_format),
+                self.isha   .minutes_to_time(&time_format)
+                )
+        
+    }
+        
+    fn print_data_as_hours_and_minutes(&self, time_format:TimeFormat){
+        println!("{}\n{}\n{}\n{}\n{}\n{}",
+                self.fajr   .minutes_to_time(&time_format),
+                self.sun    .minutes_to_time(&time_format),
+                self.dhuhur .minutes_to_time(&time_format),
+                self.asr    .minutes_to_time(&time_format),
+                self.magrib .minutes_to_time(&time_format),
+                self.isha   .minutes_to_time(&time_format)
+        )
+        
+    }
+
+    fn print_data_as_minutes(&self){
+        println!("{}\n{}\n{}\n{}\n{}\n{}",
+                self.fajr   ,
+                self.sun    ,
+                self.dhuhur ,
+                self.asr    ,
+                self.magrib ,
+                self.isha   
                 )
         
     }
@@ -42,24 +66,32 @@ impl PrayerData{
 
 
 trait TimeConversion{
-    fn minutes_to_time(self) -> String;
+    fn minutes_to_time(self,time_format:&TimeFormat) -> String;
 }
 
 impl TimeConversion for i32{
-    fn minutes_to_time(self)-> String{
+    fn minutes_to_time(self,time_format:&TimeFormat)-> String{
         let minute = &self%60;
         let mut hour = self/60;
         let mut full_time_string = "".to_owned();
-        let mut time = "am";
-        if hour > 12{
-            hour -= 12;
-            time = "pm";
+        let mut time = "";
+        
+        match time_format{
+            TimeFormat::Twelve => {
+                time = "am";
+                if hour > 12{
+                    hour -= 12;
+                    time = "pm";
+                }
+            }
+            
+            TimeFormat::TwentyFour => {}
         }
+        
         
         if hour < 10{
             full_time_string.push('0');
         }
-        
         full_time_string.push_str(&hour.to_string());
         full_time_string.push(':');
         
@@ -132,38 +164,36 @@ config contains island index
 config is stored in ~/.config/salat_mv/"
 .to_string();
     let mut time_format = TimeFormat::Twelve;
-    
-    for arg in args{
+    let mut format = Format::Normal;
+
+    for arg in &args{
         
-        if arg == "-h" || arg == "--help" {
-            println!("{}",help_message);
-            return;
-        }
-        else if arg == "-H" || arg == "--hour" {
-            time_format = TimeFormat::TwentyFour;
-        }
-        else if arg == "-o" || arg == "--output" {
-            handle_prayer_data(Format::Normal, time_format);
-            return;
-        }
-        else if arg == "-r" {
-            handle_prayer_data(Format::RawData, time_format);
-            return;
-        }
-        else if arg == "-R" {
-            handle_prayer_data(Format::RawMinuteData, time_format);
-            return;
-        }
-         else if arg == "-t" || arg == "--tui" {
+        
+        if arg == "-h" || arg == "--help"           {println!("{}",help_message);return}
+
+        else if arg == "-H" || arg == "--hour"      {time_format = TimeFormat::TwentyFour}
+
+        else if arg == "-o" || arg == "--output"    {}
+        
+        else if arg == "-r"                         {format = Format::RawData}
+        
+        else if arg == "-R"                         {format = Format::RawMinuteData}
+        
+        else if arg == "-t" || arg == "--tui" {
             tui();
             return;
-        }       
-         else if arg == "-e" || arg == "--edit" {
+        }
+        
+        else if arg == "-e" || arg == "--edit" {
             println!("select number");
             return;
+        }else{
+            println!("invalid option entred");
         }
     }
-    println!("invalid option entred");
+    
+    handle_prayer_data(format,time_format);
+
     
 
 }
@@ -173,18 +203,26 @@ enum TimeFormat {TwentyFour,Twelve}
 
 fn handle_prayer_data(output_format: Format, time_format:TimeFormat){
     
+    // gets data from file
     let backup_data:String = fs::read_to_string("/home/renderinguser/QuickAccess/Projects/codestuffz/Rust/SalatMV/src/ptdata.csv")
         .expect("READ THE data.txt FILE DAMMIT");
-       
+    
     let data:String = fs::read_to_string("./ptdata.csv").unwrap_or(backup_data);
-       
+    
+    //TODO: get data from config
+    
+    
+    // parse data for selected island
     let prayer_data : Vec<PrayerData> = data.parse_for_island(77);
     // let prayer_data :Vec<PrayerData> = parse_text_for_island(data,77);
     
     let today :usize = chrono::offset::Local::now().ordinal() as usize;
-     
     
-    prayer_data[today].print_data();
+    match output_format{
+        Format::Normal => prayer_data[today].print_data(time_format),
+        Format::RawData => prayer_data[today].print_data_as_hours_and_minutes(time_format),
+        Format::RawMinuteData => prayer_data[today].print_data_as_minutes(),
+    };
     
 }
 
