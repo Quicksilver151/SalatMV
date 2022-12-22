9cda50f056a24aef3e95f9d29e716c9a5d37f244
Default Cube <56493793+Quicksilver151@users.noreply.github.com>
2022-12-22 16:50:38 +0500

    Banished code to the shadow realm
    and some minor fixes

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,7 @@ impl PrayerData{
         val
         
     }
-            
+        
     // fn print_data(&self,time_format:TimeFormat){
     //     println!("Fajr:\t{}\nShuruq:\t{}\nDhuhur:\t{}\nAsr:\t{}\nMagrib:\t{}\nIsha:\t{}",
     //             self.fajr   .minutes_to_time(&time_format),
@@ -72,17 +72,17 @@ impl PrayerData{
     //     
     // }
 
-    fn print_data_as_minutes(&self){
-        println!("{}\n{}\n{}\n{}\n{}\n{}",
-                self.fajr   ,
-                self.sun    ,
-                self.dhuhur ,
-                self.asr    ,
-                self.magrib ,
-                self.isha   
-                )
-        
-    }
+    // fn print_data_as_minutes(&self){
+    //     println!("{}\n{}\n{}\n{}\n{}\n{}",
+    //             self.fajr   ,
+    //             self.sun    ,
+    //             self.dhuhur ,
+    //             self.asr    ,
+    //             self.magrib ,
+    //             self.isha   
+    //             )
+    //     
+    // }
 }
 
 
@@ -160,92 +160,90 @@ impl PTDataParse for String{
             
         }
         
-        full_list
-        
+        full_list   
     }
-    
 }
 
 
-fn handle_args(args: Vec<String>){
-
-    let help_message:String = 
-"SalatMV for cli
-
-Usage: pt [option]
-
-Options:
-    -h, --help       shows this help section
-    -o, --output     just outputs data (this is done by default)
-    -r               outputs raw data in hours and minutes
-    -R               outputs raw data in minutes
-    -H  --hour       show time in 24 hour format
-    -t, --tui        opens in tui mode (not implemented yet)
-    -e, --edit       edit island index (not implemented yet)
-
-config contains island index
-config is stored in ~/.config/salat_mv/"
-.to_string();
-    let mut time_format = TimeFormat::Twelve;
-    let mut format = Format::Normal;
-
-    for arg in &args{
-        
-        
-        if arg == "-h" || arg == "--help"           {println!("{}",help_message);return}
-
-        else if arg == "-H" || arg == "--hour"      {time_format = TimeFormat::TwentyFour}
-
-        else if arg == "-o" || arg == "--output"    {}
-        
-        else if arg == "-r"                         {format = Format::RawData}
-        
-        else if arg == "-R"                         {format = Format::RawMinuteData}
-        
-        else if arg == "-t" || arg == "--tui" {
-            tui();
-            return;
-        }
-        
-        else if arg == "-e" || arg == "--edit" {
-            println!("select number");
-            return;
-        }else{
-            println!("invalid option entred");
-        }
-    }
-    
-    handle_prayerdata(format,time_format);
- 
-
-}
-
-enum  Format {Normal,RawData, RawMinuteData}
-enum TimeFormat {TwentyFour,Twelve}
-
-fn handle_prayerdata(output_format: Format, time_format:TimeFormat){
-    
+// fn handle_args(args: Vec<String>){
+//
+//     let help_message:String = 
+// "SalatMV for cli
+//
+// Usage: pt [option]
+//
+// Options:
+//     -h, --help       shows this help section
+//     -o, --output     just outputs data (this is done by default)
+//     -r               outputs raw data in hours and minutes
+//     -R               outputs raw data in minutes
+//     -H  --hour       show time in 24 hour format
+//     -t, --tui        opens in tui mode (not implemented yet)
+//     -e, --edit       edit island index (not implemented yet)
+//
+// config contains island index
+// config is stored in ~/.config/salat_mv/"
+// .to_string();
+//     let mut time_format = TimeFormat::Twelve;
+//     let mut format = Format::Normal;
+//
+//     for arg in &args{
+//         
+//         
+//         if arg == "-h" || arg == "--help"           {println!("{}",help_message);return}
+//
+//         else if arg == "-H" || arg == "--hour"      {time_format = TimeFormat::TwentyFour}
+//
+//         else if arg == "-o" || arg == "--output"    {}
+//         
+//         else if arg == "-r"                         {format = Format::RawData}
+//         
+//         else if arg == "-R"                         {format = Format::RawMinuteData}
+//         
+//         else if arg == "-t" || arg == "--tui" {
+//             tui();
+//             return;
+//         }
+//         
+//         else if arg == "-e" || arg == "--edit" {
+//             println!("select number");
+//             return;
+//         }else{
+//             println!("invalid option entred");
+//         }
+//     }
+//     
+//     handle_prayerdata(format,time_format);
+//  
+//
+// }
+//
+// enum  Format {Normal,RawData, RawMinuteData}
+// enum TimeFormat {TwentyFour,Twelve}
+//
+// fn handle_prayerdata(output_format: Format, time_format:TimeFormat){
+//     
     // gets data from file
-    let backup_data:String = fs::read_to_string("/home/renderinguser/QuickAccess/Projects/codestuffz/Rust/SalatMV/src/ptdata.csv")
-        .expect("READ THE data.txt FILE DAMMIT");
-    
-    let data:String = fs::read_to_string("./ptdata.csv").unwrap_or(backup_data);
-    
+    // let backup_data:String = fs::read_to_string("/home/renderinguser/QuickAccess/Projects/codestuffz/Rust/SalatMV/src/ptdata.csv")
+    //     .expect("READ THE data.txt FILE DAMMIT");
+    // 
+    // let data:String = fs::read_to_string("./ptdata.csv").unwrap_or(backup_data);
+    // 
     
     
     // parse data for selected island
-    let prayer_data : Vec<PrayerData> = data.parse_for_island(77);
+    // let prayer_data : Vec<PrayerData> = data.parse_for_island(77);
     // let prayer_data :Vec<PrayerData> = parse_text_for_island(data,77);
     
-    let today :usize = chrono::offset::Local::now().ordinal() as usize;
+    // let today :usize = chrono::offset::Local::now().ordinal() as usize;
     
     // match output_format{
     //     Format::Normal => prayer_data[today].print_data(time_format),
     //     Format::RawData => prayer_data[today].print_data_as_hours_and_minutes(time_format),
     //     Format::RawMinuteData => prayer_data[today].print_data_as_minutes(),
     // };
-    
-}
+    // 
+// }
 
 // TODO::::::::::::::::::;
 fn tui(){
@@ -283,7 +281,7 @@ fn handle_prayer_data(flag:Flag, cfg:Config){
     for (i,pt) in pt_vec.iter().enumerate(){
         match flag.disp{
             DispType::Normal => print!("{}:\t",names[i]),
-            DispType::Raw    => print!("{}",i),
+            DispType::Raw    => print!(""),
         }
         match flag.output{
             OutType::Hours   => println!("{}",pt.minutes_to_time(&flag.time)),
