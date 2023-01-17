045e6e774e9a7ed875a2b1d29a9ba7d7b2a51cc0
Default Cube <56493793+Quicksilver151@users.noreply.github.com>
2023-01-17 08:22:53 +0500

    lmao, had put current day fetch outside the loop

--- src/main.rs
+++ src/main.rs
@@ -323,13 +323,12 @@ fn edit(){
 
 fn active(prayer_data: Vec<PrayerData>, flag: &Flag){
     
-    let today: usize = chrono::offset::Local::now().ordinal() as usize - 1;
-    
+        
     new_buffer();
     
     // active loop
     loop{
-        
+        let today: usize = chrono::offset::Local::now().ordinal() as usize - 1;
         let pt_vec = prayer_data[today].vec_from_island_set();
         let current_time = get_current_time_in_minutes() as i32;
         let (_,_,seconds,_) = get_current_time(&flag.time);
